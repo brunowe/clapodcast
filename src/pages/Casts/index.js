e0b15986faa7d556edf26296676d9e600be21a11
Bruno Weber de Castro <brunoweber500@gmail.com>
2020-05-02 22:34:24 -0300

    FIX / CONFIG / Cria rota /casts

--- src/pages/Casts/index.js
+++ src/pages/Casts/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Content, Bg } from "./styles";
 
-// import { Container } from './styles';
-
-export default class Home extends React.Component {
+export default class Casts extends React.Component {
   render() {
     return (
       <Bg>
         <Content>
           <h1>Olá Mundo! Esta será a página do Clã Podcast!</h1>
           <h2>Ou seja lá o nome que a gente for dar pra isso.</h2>
+          <h4>(e se funcionar)</h4>
         </Content>
       </Bg>
     );
